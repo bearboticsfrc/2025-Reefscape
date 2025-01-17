b9b6b248d87a0b31bd21cac879043a0f0128e3e4
Mason A Paprzycki <mason44.pap@gmail.com>
2025-01-16 18:36:02 -0700

    spotless

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -2,46 +2,45 @@ package frc.robot.constants;
 
 public final class VisionConstants {
 
-    public enum TAG {
-        BLUE_FRONT_REEF(18),
-        BLUE_R1_REEF(17),
-        BLUE_R2_REEF(22),
-        BLUE_L1_REEF(19),
-        BLUE_L2_REEF(20),
-        BLUE_BACK_REEF(21),
+  public enum TAG {
+    BLUE_FRONT_REEF(18),
+    BLUE_R1_REEF(17),
+    BLUE_R2_REEF(22),
+    BLUE_L1_REEF(19),
+    BLUE_L2_REEF(20),
+    BLUE_BACK_REEF(21),
 
-        RED_FRONT_REEF(7),
-        RED_R1_REEF(8),
-        RED_R2_REEF(9),
-        RED_L1_REEF(6),
-        RED_L2_REEF(11),
-        RED_BACK_REEF(10),
+    RED_FRONT_REEF(7),
+    RED_R1_REEF(8),
+    RED_R2_REEF(9),
+    RED_L1_REEF(6),
+    RED_L2_REEF(11),
+    RED_BACK_REEF(10),
 
-        BLUE_BARGE_FRONT(14),
-        BLUE_BARGE_BACK(4),
-        
-        RED_BARGE_FRONT(5),
-        RED_BARGE_BACK(15),
+    BLUE_BARGE_FRONT(14),
+    BLUE_BARGE_BACK(4),
 
-        BLUE_PROC(3),
-        RED_PROC(16),
+    RED_BARGE_FRONT(5),
+    RED_BARGE_BACK(15),
 
-        BLUE_COR_STAT_L(13),
-        BLUE_COR_STAT_R(12),
+    BLUE_PROC(3),
+    RED_PROC(16),
 
-        RED_COR_STAT_L(1),
-        RED_COR_STAT_R(2),
-        ;
-    
-        public final int tagNumber;
-    
-        public int getValue() {
-          return tagNumber;
-        }
-    
-        private TAG(int tagNumber) {
-          this.tagNumber = tagNumber;
-        }
-      }
-    
+    BLUE_COR_STAT_L(13),
+    BLUE_COR_STAT_R(12),
+
+    RED_COR_STAT_L(1),
+    RED_COR_STAT_R(2),
+    ;
+
+    public final int tagNumber;
+
+    public int getValue() {
+      return tagNumber;
+    }
+
+    private TAG(int tagNumber) {
+      this.tagNumber = tagNumber;
+    }
+  }
 }
--- src/main/java/frc/robot/location/FieldPositions.java
+++ src/main/java/frc/robot/location/FieldPositions.java
@@ -1,124 +1,114 @@
 package frc.robot.location;
 
+import bearlib.fms.AllianceColor;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFieldLayout.OriginPosition;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-
 import java.io.IOException;
-import java.util.Arrays;
 
-import bearlib.fms.AllianceColor;
+public class FieldPositions {
 
-public class FieldPositions{
-    
-    public static double FIELD_LENGTH = 17.55; // meters
-    public static double FIELD_WIDTH = 8.05; // meters
-    
-    private AprilTagFieldLayout layout;
-    private static FieldPositions instance = null;
+  public static double FIELD_LENGTH = 17.55; // meters
+  public static double FIELD_WIDTH = 8.05; // meters
 
-    public FieldPositions(){
-        initializeLayout();
+  private AprilTagFieldLayout layout;
+  private static FieldPositions instance = null;
+
+  public FieldPositions() {
+    initializeLayout();
+  }
+
+  public static FieldPositions getInstance() {
+    if (instance == null) {
+      instance = new FieldPositions();
     }
+    return instance;
+  }
 
-    public static FieldPositions getInstance() {
-        if (instance == null) {
-          instance = new FieldPositions();
-        }
-        return instance;
-      }
-    
-    private void initializeLayout() {
-        try {
-          layout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2025Reefscape.m_resourceFile);
-          layout.setOrigin(OriginPosition.kBlueAllianceWallRightSide);
-        } catch (IOException e) {
-          DriverStation.reportError("Failed to load AprilTagFieldLayout", e.getStackTrace());
-        }
-      }
-
-    public AprilTagFieldLayout getLayout() {
-        return layout;
+  private void initializeLayout() {
+    try {
+      layout = AprilTagFieldLayout.loadFromResource(AprilTagFields.k2025Reefscape.m_resourceFile);
+      layout.setOrigin(OriginPosition.kBlueAllianceWallRightSide);
+    } catch (IOException e) {
+      DriverStation.reportError("Failed to load AprilTagFieldLayout", e.getStackTrace());
     }
+  }
 
+  public AprilTagFieldLayout getLayout() {
+    return layout;
+  }
 
+  private static double inchesToMeters(double inches) {
+    return inches * 0.0254;
+  }
 
+  private static double DIST_TO_PERIM = inchesToMeters(130);
+  private static double WIDTH_REEF = inchesToMeters(93.5);
 
+  private static double REEF_SIDE_LENG = Math.sqrt(Math.pow(WIDTH_REEF, 2) / 2);
+  private static double ANG_BETW_SIDE = Math.toRadians(120);
 
+  /**
+   * Enum for the reef reference points. Points follow a counter-clockwise order starting with the
+   * lowest and furthest left on the blue side. Starting with the highest and furthest right on the
+   * red side. this way the points will function as way points on both the red and blue sides
+   */
 
+  // since this class will only need to be constructed
+  public enum ReefReferencePoint {
+    REF_POINT_ONE(new double[] {DIST_TO_PERIM, (FIELD_WIDTH / 2) - (REEF_SIDE_LENG / 2)}),
 
-
-
-    private static double inchesToMeters(double inches) {
-        return inches * 0.0254;
-    }
-
-    private static double DIST_TO_PERIM = inchesToMeters(130);
-    private static double WIDTH_REEF = inchesToMeters(93.5);
-
-    private static double REEF_SIDE_LENG = Math.sqrt(Math.pow(WIDTH_REEF, 2)/2);
-    private static double ANG_BETW_SIDE = Math.toRadians(120);
-
-    /**
-     * Enum for the reef reference points.
-     * Points follow a counter-clockwise order 
-     * starting with the lowest and furthest left on the blue side.
-     * Starting with the highest and furthest right on the red side.
-     * this way the points will function as way points on both the red and blue sides
-     */
-
-     // since this class will only need to be constructed 
-    public enum ReefReferencePoint {
-        REF_POINT_ONE(new double[]{
-            DIST_TO_PERIM,
-            (FIELD_WIDTH / 2) - (REEF_SIDE_LENG / 2)
+    REF_POINT_TWO(
+        new double[] {
+          REF_POINT_ONE.getRefCoordinates()[0] + Math.sin(ANG_BETW_SIDE) * REEF_SIDE_LENG,
+          REF_POINT_ONE.getRefCoordinates()[1] + Math.cos(ANG_BETW_SIDE) * REEF_SIDE_LENG
         }),
-    
-        REF_POINT_TWO(new double[]{
-            REF_POINT_ONE.getRefCoordinates()[0] + Math.sin(ANG_BETW_SIDE) * REEF_SIDE_LENG,
-            REF_POINT_ONE.getRefCoordinates()[1] + Math.cos(ANG_BETW_SIDE) * REEF_SIDE_LENG
+
+    REF_POINT_THREE(
+        new double[] {
+          REF_POINT_TWO.getRefCoordinates()[0] + Math.sin(ANG_BETW_SIDE) * REEF_SIDE_LENG,
+          REF_POINT_ONE.getRefCoordinates()[1]
         }),
-    
-        REF_POINT_THREE(new double[]{
-            REF_POINT_TWO.getRefCoordinates()[0] + Math.sin(ANG_BETW_SIDE) * REEF_SIDE_LENG,
-            REF_POINT_ONE.getRefCoordinates()[1]
+
+    REF_POINT_FOUR(
+        new double[] {
+          REF_POINT_THREE.getRefCoordinates()[0],
+          REF_POINT_ONE.getRefCoordinates()[1] + REEF_SIDE_LENG
         }),
-    
-        REF_POINT_FOUR(new double[]{
-            REF_POINT_THREE.getRefCoordinates()[0],
-            REF_POINT_ONE.getRefCoordinates()[1] + REEF_SIDE_LENG
+
+    REF_POINT_FIVE(
+        new double[] {
+          REF_POINT_FOUR.getRefCoordinates()[0] - Math.sin(ANG_BETW_SIDE) * REEF_SIDE_LENG,
+          REF_POINT_FOUR.getRefCoordinates()[1] - Math.cos(ANG_BETW_SIDE) * REEF_SIDE_LENG
         }),
-    
-        REF_POINT_FIVE(new double[]{
-            REF_POINT_FOUR.getRefCoordinates()[0] - Math.sin(ANG_BETW_SIDE) * REEF_SIDE_LENG,
-            REF_POINT_FOUR.getRefCoordinates()[1] - Math.cos(ANG_BETW_SIDE) * REEF_SIDE_LENG
-        }),
-    
-        REF_POINT_SIX(new double[]{
-            REF_POINT_ONE.getRefCoordinates()[0],
-            REF_POINT_ONE.getRefCoordinates()[1] + REEF_SIDE_LENG
+
+    REF_POINT_SIX(
+        new double[] {
+          REF_POINT_ONE.getRefCoordinates()[0],
+          REF_POINT_ONE.getRefCoordinates()[1] + REEF_SIDE_LENG
         });
-    
-        private final double[] coordinates;
-    
-        ReefReferencePoint(double[] coordinates) {
-            this.coordinates = coordinates;
-        }
-    
-        private double[] getRefCoordinates() {
-            return coordinates;
-        }
-        public Pose2d getPose(){
-      
-            if(AllianceColor.getAlliance()==Alliance.Red){
-                return new Pose2d(FIELD_LENGTH-coordinates[0],FIELD_WIDTH-coordinates[1], new Rotation2d());
-        }
-            return new Pose2d(coordinates[0],coordinates[1], new Rotation2d());
-        }
+
+    private final double[] coordinates;
+
+    ReefReferencePoint(double[] coordinates) {
+      this.coordinates = coordinates;
     }
+
+    private double[] getRefCoordinates() {
+      return coordinates;
+    }
+
+    public Pose2d getPose() {
+
+      if (AllianceColor.getAlliance() == Alliance.Red) {
+        return new Pose2d(
+            FIELD_LENGTH - coordinates[0], FIELD_WIDTH - coordinates[1], new Rotation2d());
+      }
+      return new Pose2d(coordinates[0], coordinates[1], new Rotation2d());
+    }
+  }
 }
