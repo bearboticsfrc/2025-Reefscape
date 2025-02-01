a65cdef2c7e48ff37c8685d3ae3edb3138b1d32a
robertmakowsky <robmakowsky@icloud.com>
2025-02-01 11:56:43 -0700

    added documentation

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -1,6 +1,7 @@
 package frc.robot.constants;
 
 public class ElevatorConstants {
+
   // TODO: Add constants for elevator heights
   public static final double HOME = -1.0;
   public static final double L1 = -2.0;
@@ -8,8 +9,8 @@ public class ElevatorConstants {
   public static final double L3 = -4.0;
   public static final double L4 = -5.0;
 
-  public static final double MAX_VELOCITY = 0;
-  public static final double MAX_ACCELERATION = 0;
+  public static final double MAX_VELOCITY = 0.0;
+  public static final double MAX_ACCELERATION = 0.0;
 
   // TODO: Add constants for elevator feedforward
   public static final double KA = -1.0;
--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -5,7 +5,6 @@ import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkMax;
 import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -18,16 +17,20 @@ import java.io.IOException;
 public class ElevatorSubsystem extends SubsystemBase {
 
   private ElevatorFeedforward elevatorFeedforward =
-      new ElevatorFeedforward(ElevatorConstants.KS, ElevatorConstants.KG, ElevatorConstants.KS, ElevatorConstants.KV);
+      new ElevatorFeedforward(
+          ElevatorConstants.KS, ElevatorConstants.KG, ElevatorConstants.KS, ElevatorConstants.KV);
   private ShuffleboardTab elevatorTab;
   private SparkBase leaderMotor;
   private RelativeEncoder leaderRelativeEncoder;
   private SparkClosedLoopController elevatorPIDController;
   private SparkBase followerMotor;
   private RelativeEncoder followerRelativeEncoder;
-  private double elevatorState = ElevatorConstants.HOME;
+  private double currentElevatorState = ElevatorConstants.HOME;
   private final boolean SHUFFLEBOARD_ENABLED = true;
 
+  /*
+   * constructor for the elevator subsystem
+   */
   public ElevatorSubsystem() {
     elevatorTab = Shuffleboard.getTab("Elevator");
     configureMotors();
@@ -36,6 +39,10 @@ public class ElevatorSubsystem extends SubsystemBase {
     }
   }
 
+  /*
+   * configures the both  of motors for the elevator subsystem
+   * along with their pid controllers and encoders
+   */
   private void configureMotors() {
     File directory = new File(Filesystem.getDeployDirectory(), "Elevator");
 
@@ -44,7 +51,7 @@ public class ElevatorSubsystem extends SubsystemBase {
           new MotorParser(directory)
               .withMotor("leaderMotor.json")
               .withEncoder("leaderEncoder.json")
-              .withPidf("leaderPidf.json");
+              .withPidf("leaderPidf.json", 0);
 
       leaderMotor = elevatorLeaderMotorParser.configure().getSpark();
 
@@ -52,9 +59,9 @@ public class ElevatorSubsystem extends SubsystemBase {
           new MotorParser(directory)
               .withMotor("followerMotor.json")
               .withEncoder("followerEncoder.json")
-              .withPidf("followerPidf.json");
+              .withPidf("followerPidf.json", 0);
 
-      followerMotor = elevatorFollowerMotorParser.configure().getSpark(); 
+      followerMotor = elevatorFollowerMotorParser.configure().getSpark();
 
     } catch (IOException e) {
       e.printStackTrace();
@@ -65,45 +72,65 @@ public class ElevatorSubsystem extends SubsystemBase {
     elevatorPIDController = leaderMotor.getClosedLoopController();
   }
 
+  /*
+   * configures the shuffleboard tab for the elevator subsystem and adds some values to it
+   */
   private void setupShuffleboardTab() {
-    elevatorTab.add("position", elevatorState);
+    elevatorTab.add("position", currentElevatorState);
     elevatorTab.addDouble("leader encoder position", leaderRelativeEncoder::getPosition);
     elevatorTab.addDouble("follower encoder position", followerRelativeEncoder::getPosition);
   }
 
-  public void setElevatorReference() {
-    elevatorPIDController.setReference(elevatorState, SparkBase.ControlType.kPosition, ClosedLoopSlot.kSlot0, elevatorFeedforward.calculate(ElevatorConstants.MAX_VELOCITY,ElevatorConstants.MAX_ACCELERATION));
-
+  /*
+   * sets the elevator reference to a target state
+   * @param TargetElevatorState the target state for the elevator
+   */
+  public void setElevatorReference(double TargetElevatorState) {
+    elevatorPIDController.setReference(
+        TargetElevatorState,
+        SparkBase.ControlType.kPosition,
+        ClosedLoopSlot.kSlot0,
+        elevatorFeedforward.calculate(
+            ElevatorConstants.MAX_VELOCITY, ElevatorConstants.MAX_ACCELERATION));
   }
 
+  /*
+   * sets the elevator to the home position
+   */
   public void setElevatorHome() {
-    elevatorState = ElevatorConstants.HOME;
-    setElevatorReference();
-  
+    setElevatorReference(ElevatorConstants.HOME);
+    currentElevatorState = ElevatorConstants.HOME;
   }
 
+  /*
+   * sets the elevator to the reef level one position
+   */
   public void setElevatorL1() {
-    elevatorState = ElevatorConstants.L1;
-    setElevatorReference();
-  
+    setElevatorReference(ElevatorConstants.L1);
+    currentElevatorState = ElevatorConstants.L1;
   }
 
+  /*
+   * sets the elevator to the reef level two position
+   */
   public void setElevatorL2() {
-    elevatorState = ElevatorConstants.L2;
-    setElevatorReference();
-
+    setElevatorReference(ElevatorConstants.L2);
+    currentElevatorState = ElevatorConstants.L2;
   }
 
+  /*
+   * sets the elevator to the reef level three position
+   */
   public void setElevatorL3() {
-    elevatorState = ElevatorConstants.L3;
-    setElevatorReference();
-
+    setElevatorReference(ElevatorConstants.L3);
+    currentElevatorState = ElevatorConstants.L3;
   }
 
+  /*
+   * sets the elevator to the reef level four position
+   */
   public void setElevatorL4() {
-    elevatorState = ElevatorConstants.L4;
-    setElevatorReference();
-  
+    setElevatorReference(ElevatorConstants.L4);
+    currentElevatorState = ElevatorConstants.L4;
   }
-
 }
