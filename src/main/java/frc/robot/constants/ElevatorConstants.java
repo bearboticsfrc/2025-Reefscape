b0d854adfc948f2d63fac93e21f36c38b0f98c3c
robertmakowsky <robmakowsky@icloud.com>
2025-02-01 14:02:42 -0700

    Update ElevatorConstants.java

--- src/main/java/frc/robot/constants/ElevatorConstants.java
+++ src/main/java/frc/robot/constants/ElevatorConstants.java
@@ -2,19 +2,21 @@ package frc.robot.constants;
 
 public class ElevatorConstants {
 
-  // TODO: Add constants for elevator heights
-  public static final double HOME = -1.0;
-  public static final double L1 = -2.0;
-  public static final double L2 = -3.0;
-  public static final double L3 = -4.0;
-  public static final double L4 = -5.0;
+  
+  public static final double HOME = 0.0;
+  
+  // TODO: Add constants for elevator heights in rotations
+  public static final double L1 = 0.0;
+  public static final double L2 = 0.0;
+  public static final double L3 = 0.0;
+  public static final double L4 = 0.0;
 
-  public static final double MAX_VELOCITY = 0.0;
-  public static final double MAX_ACCELERATION = 0.0;
+  public static final double MAX_VELOCITY = 0.3;
+  public static final double MAX_ACCELERATION = 0.3;
 
-  // TODO: Add constants for elevator feedforward
-  public static final double KA = -1.0;
-  public static final double KS = -1.0;
-  public static final double KG = -1.0;
-  public static final double KV = -1.0;
+  // TODO: Add coefficient of static friction for elevator feedforward
+  public static final double KA = 0.41;
+  public static final double KS = 0.0;
+  public static final double KG = 2.26;
+  public static final double KV = 3.08;
 }
