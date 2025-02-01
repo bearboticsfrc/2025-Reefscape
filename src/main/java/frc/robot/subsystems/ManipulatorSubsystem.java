80f1d9939afa1798422d8fa09004024208b213e9
Mason A Paprzycki <93019015+MasonPaprzycki@users.noreply.github.com>
2025-02-01 12:00:24 -0700

    Update src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
    Co-authored-by: Kellen <70085049+Kellenn@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ManipulatorSubsystem.java
@@ -3,7 +3,7 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ManipulatorSubsystem extends SubsystemBase {
-  private final CoralSubsystem intake;
+  private final CoralSubsystem coral;
 
   public ManipulatorSubsystem() {
     this.intake = new CoralSubsystem();
