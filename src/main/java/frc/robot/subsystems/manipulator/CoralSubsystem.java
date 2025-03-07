c44ff62841d9fe5661ec0c9390439928c3689a87
Kellen <70085049+Kellenn@users.noreply.github.com>
2025-03-06 19:56:56 -0700

    Add intake wheel + motor

--- src/main/java/frc/robot/subsystems/manipulator/CoralSubsystem.java
+++ src/main/java/frc/robot/subsystems/manipulator/CoralSubsystem.java
@@ -22,6 +22,9 @@ public class CoralSubsystem extends SubsystemBase {
   private final Time SCORING_TIME = Seconds.of(0.5);
   private final double RETRACT_THRESHOLD = -0.85;
 
+  @Logged(name = "Coral Intake Motor")
+  private final SparkBase intake;
+
   @Logged(name = "Coral Outake Motor")
   private final SparkBase outake;
 
@@ -39,6 +42,7 @@ public class CoralSubsystem extends SubsystemBase {
     File directory = new File(Filesystem.getDeployDirectory(), "motors/coral");
 
     try {
+      intake = new MotorParser(directory).withMotor("intake.json").configureAsync();
       outake = new MotorParser(directory).withMotor("outake.json").configureAsync();
     } catch (IOException exception) {
       throw new RuntimeException("Failed to configure coral motor(s)!", exception);
@@ -62,6 +66,7 @@ public class CoralSubsystem extends SubsystemBase {
    */
   public Command intakeCoral() {
     return runOutake(MotorSpeed.REVERSE_TENTH)
+        .alongWith(runIntake(MotorSpeed.TENTH))
         .andThen(Commands.waitUntil(this::hasCoral))
         .andThen(Commands.runOnce(() -> outakeEncoder.setPosition(0)))
         .andThen(Commands.waitUntil(() -> outakeEncoder.getPosition() <= RETRACT_THRESHOLD))
@@ -82,7 +87,7 @@ public class CoralSubsystem extends SubsystemBase {
   /**
    * Run the coral outake at the supplied speed.
    *
-   * @param speed {@link MotorSpeed} describing the desired intake motor speed.
+   * @param speed {@link MotorSpeed} describing the desired outake motor speed.
    * @return A {@link Command} running the coral intake.
    */
   private Command runOutake(MotorSpeed speed) {
@@ -90,9 +95,19 @@ public class CoralSubsystem extends SubsystemBase {
   }
 
   /**
+   * Run the intake outake at the supplied speed.
+   *
+   * @param speed {@link MotorSpeed} describing the desired intake motor speed.
+   * @return A {@link Command} running the coral intake.
+   */
+  private Command runIntake(MotorSpeed speed) {
+    return Commands.runOnce(() -> intake.set(speed.getSpeed()));
+  }
+
+  /**
    * @return A {@link Command} stopping both the intake and outake motors.
    */
   public Command stop() {
-    return Commands.runOnce(outake::stopMotor);
+    return Commands.runOnce(outake::stopMotor).alongWith(Commands.runOnce(intake::stopMotor));
   }
 }
